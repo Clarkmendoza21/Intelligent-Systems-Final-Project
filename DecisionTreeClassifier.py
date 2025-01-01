5005afc5c3a5fb08905c697f3f9a66a039dce572
Zymon Clark Mendoza <zymonclark.mendoza@gmail.com>
2025-01-01 20:52:37 +0800

    ADD Decision Tree Classifier Algorithm
    edit Random Forest spacings

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 
@@ -17,18 +17,17 @@ X['Gender'] = le.fit_transform(X['Gender'])
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a Random Forest Classifier
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+# Create a Decision Tree Classifier
+clf = DecisionTreeClassifier()
 
 # Train the model
-rf_classifier.fit(X_train, y_train)
+clf.fit(X_train, y_train)
 
 # Make predictions on the test set
-y_pred = rf_classifier.predict(X_test)
+y_pred = clf.predict(X_test)
 
 # Evaluate the model's accuracy
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
-
+print("Accuracy:", accuracy)
 print("Classification Report:\n", classification_report(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
